91d121d587f339c348038c34ff2fdc642a425d96
Luke Liukonen <liukonen@gmail.com>
2020-08-04 19:26:19 -0500

    Update the UI converting for consistancy

--- js/luke.js
+++ js/luke.js
@@ -1,22 +1,5 @@
-function generate(){
-  //console.log("generate");
-  //let MainContainer = $("#mainContainer");
-  //let alter = true;
-  //let items = JSON.parse(getFile("./json/content.json"))["items"];
-  //let template = $("#itemTemplate");
-  //let template2 = $("#itemTemplate2");
-  //items.forEach(function(item){
-  //  if (alter){ template.tmpl(item).appendTo(MainContainer);}
-  //  else{template2.tmpl(item).appendTo(MainContainer);}
-  //  alter =  !alter;
-  //});
-
-}
-
 function DetermButtonColor(release){return (release.startsWith("https://liukonen.github.io")) ? "success" : "primary";}
 function DetermButtonName(release){return release.startsWith("https://liukonen.github.io")? "Website" : "Release";}
-
-generate();
 
 /*region Lazy Load */
 function preloadImg(img){
