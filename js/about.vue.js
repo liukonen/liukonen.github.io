0419f2caac465d3c59246743079022316ca576bb
Luke Liukonen <liukonen@gmail.com>
2020-08-05 12:34:40 -0500

    upgrade about page to vue, cleanup

---
+++ js/about.vue.js
@@ -0,0 +1,113 @@
+/*Vue Components*/
+Vue.component('Knowledge', {
+    props:['item'],
+    methods:{
+        GetClass: function (item) {
+            switch(item.level){
+                case 3:
+                case 2: return "badge badge-pill badge-primary bg-purple";
+                default: return "badge badge-pill badge-info";
+              }
+        },
+        GetName: function (item) {
+            switch(item.level){
+                case 3:
+                case 2: return "Professional";
+                default: return "Hobbyist";
+              }
+        }
+    }
+    ,
+    template: `
+    <div class="col2 mr-5 text-center lateloadfadeIn">
+    <a v-bind:href="item.link" target="blank" v-bind:alt="item.title"><img v-bind:data-src="item.img" v-bind:title="item.title" width="100px" height="100px" style="display:none;" /></a>
+    <p>{{item.title}}</p>
+    <p>
+      <span v-bind:class="GetClass(item)">{{GetName(item.level)}}</span>
+    </p>
+  </div>`
+  })
+
+Vue.component('Work', {
+    props:['job'],
+    template: `        
+    <div class="col-10 mt-3 mx-auto">
+    <div class="card border-0">
+      <div class="card-horizontal">
+        <div class="img-square-wrapper">
+          <img class="ImgRoundCorner border-right shadow" v-bind:src="job.img" v-bind:alt="job.name"></img>
+        </div>
+        <div class="card-body">
+          <h4 class="card-title">{{job.name}}</h4>
+          <h5>{{job.title}}, {{job.timeworked}}</h5>
+          <p class="card-text">{{job.summary}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+    `
+})
+
+
+function WPCheck(){
+    var elem = document.createElement('canvas');
+    if (!!(elem.getContext && elem.getContext('2d'))) {
+        let CanDo = elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+        return CanDo;
+    }
+    return false;
+}
+
+var sourceData = JSON.parse(getFile("./json/about.json"));
+var WP = WPCheck;
+
+
+//Vue.use(VueLazyload, {preLoad: 0, attempt: 1})
+
+  
+career = new Vue({
+    el:"#career",
+    data: {
+        Work: sourceData.Work,
+        WepPSupport: WP,
+        highlights: sourceData.highlights,
+        school: sourceData.School
+    }, methods:{}
+  });
+
+backend = new Vue({el:"#vbackend",data:{Ex: sourceData.Experence, WepPSupport: WP}});
+welcome = new Vue({el:"#welcome", data:{item: sourceData.welcome}});
+About = new Vue({el:"#about", data:{Hobbies: sourceData.Hobbies, Vol: sourceData.volunteer}});
+
+
+/**Vanilla JS */
+const lateLoadFadeInObserver = new IntersectionObserver((entries, lateLoadFadeInObserver)=>{
+    entries.forEach(entry =>{
+      if (entry.isIntersecting){
+        lateLoadFadeInObserver.unobserve(entry.target);
+        let I = $(entry.target);
+        let img = I.find("img");
+        let dsrc = img.attr("data-src");
+        img.attr("src",  SupportsWebp != 1 ? dsrc + ".png" : dsrc);
+        img.fadeIn("slow");
+      }
+    });
+  },{threshold: 1, rootMargin:"0px 0px 0px 0px"});
+  document.querySelectorAll(".lateloadfadeIn").forEach(entry =>{lateLoadFadeInObserver.observe(entry);});
+  
+  
+  /*region transparent Navbar */
+  const header = document.querySelector("nav");
+  const navObserver = new IntersectionObserver((entries, navObserver) =>{
+    entries.forEach(entry =>{
+      if (!entry.isIntersecting){
+        header.classList.add("bg-dark");
+        header.classList.add("transition");
+      }else{
+        header.classList.remove("bg-dark");
+        header.classList.remove("transition");
+      }
+    });
+  }, {rootMargin:"-250px 0px 0px 0px"});
+  navObserver.observe(document.querySelector(".bgimg-1"));
+  /*end region*/
--- js/index.vue.js
+++ js/index.vue.js
@@ -23,6 +23,10 @@ vm = new Vue({
                 return "col-md-7 order-md-1 fade-in";
             }
         },
+        GetClass(valueType){
+            if (valueType == 0){return "col-md-5 fade-in"};
+            return "col-md-7 fade-in";
+        },
         DetermButtonColor(ItemType){
             if (ItemType.toLowerCase() == "website") {return "btn btn-success shadow ml-2";}
             if (ItemType.toLowerCase() == "release") {return "btn btn-primary shadow ml-2";}
@@ -50,3 +54,51 @@ vm = new Vue({
     T.animate({'opacity':'1'},1500);
   })
  
+  /*region Lazy Load */
+function preloadImg(img){
+    let src = img.getAttribute("data-src"); 
+    if (src){
+      if (SupportsWebp != 1){img.src = src + ".png";}
+      else{img.src = src;} 
+      console.log(src);
+    }
+  }
+  
+  const imgObserver = new IntersectionObserver((entries, imgObserver)=> {
+      entries.forEach(entry => {
+        if (entry.isIntersecting){
+          preloadImg(entry.target);
+          imgObserver.unobserve(entry.target);  
+        }
+  });
+  }, {threshold:1, rootMargin:"0px 0px 300px 0px"});
+  document.querySelectorAll(".featurette-image").forEach(img => {imgObserver.observe(img);});
+  
+  /*end region - lazy load*/
+  
+  /*region - navbar transparent effect*/
+  const header = document.querySelector(".navbar-brand");
+  const navObserver = new IntersectionObserver((entries, navObserver) =>{
+    entries.forEach(entry =>{
+      if (!entry.isIntersecting){
+        header.parentElement.classList.add("bg-dark");
+        header.parentElement.classList.add("transition");
+      }else{
+        header.parentElement.classList.remove("bg-dark");
+        header.parentElement.classList.remove("transition");
+      }
+    });
+  }, {rootMargin:"-50px 0px 0px 0px"});
+  navObserver.observe(document.querySelector(".jumbotron"));
+  
+  /*end region navbar transparent effect */
+  
+  
+  /*region fader */
+  const faderObsever = new IntersectionObserver((entries, faderObsever) => {
+   entries.forEach(entry =>{
+     if (entry.isIntersecting){$(entry.target).animate({'opacity':'1'},500); faderObsever.unobserve(entry.target); }
+   });
+  }, {rootMargin:"0px 0px 0px 0px"});
+  document.querySelectorAll(".fade-in").forEach(item => faderObsever.observe(item));
+  /*end region fader */
