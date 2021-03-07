97c15f5958bab17e87728e275b58cd5679599bd3
luke liukonen <liukonen@gmail.com>
2021-03-07 17:34:07 -0600

    Fix card height, cleanup styling of code

--- js/vue.js
+++ js/vue.js
@@ -1,92 +1,125 @@
-Vue.use(VueLazyload, { preLoad: 0, attempt: 1 })
+Vue.use(VueLazyload, {
+  preLoad: 0,
+  attempt: 1,
+});
 
 var SupportsWebp = 2; // 2- not set, 1, true, 0, false
 var pageObject = JSON.parse(getFile("./json/page.json"));
 
-
 function getFile(fileUrl) {
   let temp;
   $.ajax({
-    url: fileUrl, dataType: "text", async: false,
-    error: (jqXHR, textStatus, error) => { window.alert(error); },
-    success: (data, textStatus, jqXHR) => { temp = data; }
+    url: fileUrl,
+    dataType: "text",
+    async: false,
+    error: (jqXHR, textStatus, error) => {
+      window.alert(error);
+    },
+    success: (data, textStatus, jqXHR) => {
+      temp = data;
+    },
   });
   return temp;
 }
 
-$(window).on('load', function () {
+$(window).on("load", function () {
   console.log("hit");
   switch (SupportsWebp) {
     case 1:
     case 0:
       break;
     default:
-      let elem = document.createElement('canvas');
-      if (!!(elem.getContext && elem.getContext('2d')) && elem.toDataURL('image/webp').indexOf('data:image/webp') == 0) {
+      let elem = document.createElement("canvas");
+      if (
+        !!(elem.getContext && elem.getContext("2d")) &&
+        elem.toDataURL("image/webp").indexOf("data:image/webp") == 0
+      ) {
         SupportsWebp = 1;
-      } else { SupportsWebp = 0; }
+      } else {
+        SupportsWebp = 0;
+      }
   }
   if (SupportsWebp == 0) {
     console.log("WEBP Images NOT Supported");
     let imgs = $("img").toArray();
     imgs.forEach((img) => {
       var src = $(img).attr("src");
-      if (typeof src !== typeof undefined && src !== false && src != "" && src.substring(src.length - 4) == "webp") {
+      if (
+        typeof src !== typeof undefined &&
+        src !== false &&
+        src != "" &&
+        src.substring(src.length - 4) == "webp"
+      ) {
         $(img).attr("src", src + ".png");
       }
     });
     $(".bgimg-1").css("background-image", "url('../img/jumbotron.webp.png')");
-  } else { console.log("WEBP Images Supported"); }
-
+  } else {
+    console.log("WEBP Images Supported");
+  }
 });
 
-if (navigator.appName.indexOf("Internet Explorer") != -1 || navigator.userAgent.match(/Trident.*rv[ :]*11\./)) { window.location = "ie.html"; }
+if (
+  navigator.appName.indexOf("Internet Explorer") != -1 ||
+  navigator.userAgent.match(/Trident.*rv[ :]*11\./)
+) {
+  window.location = "ie.html";
+}
 
 /*Vue Components*/
-Vue.component('alist', {
+Vue.component("alist", {
   props: {
     list: { type: Array },
-    styles: { type: Number, default: 0 }
+    styles: { type: Number, default: 0 },
   },
   methods: {
     GetStyle(number) {
-      return (number == 1) ? "list-group text-dark shadow" : "list-group";
-    }
+      return number == 1 ? "list-group text-dark shadow" : "list-group";
+    },
   },
   template: `
   <ul :class="GetStyle(styles)">
    <li v-for="item in list" class="list-group-item list-group-item-secondary">{{item.a}}</li>
   </ul>
-  `
-})
+  `,
+});
 
-Vue.component('Knowledge', {
-  props: ['item'],
+Vue.component("Knowledge", {
+  props: ["item"],
   methods: {
     GetClass: function (item) {
       switch (item.level) {
         case 3:
-        case 2: return "badge badge-pill badge-primary bg-purple";
-        default: return "badge badge-pill badge-info";
+        case 2:
+          return "badge badge-pill badge-primary bg-purple";
+        default:
+          return "badge badge-pill badge-info";
       }
     },
     GetName: function (item) {
       switch (item.level) {
         case 3:
-        case 2: return "Professional";
-        default: return "Hobbyist";
+        case 2:
+          return "Professional";
+        default:
+          return "Hobbyist";
       }
-    }
+    },
   },
   template: `
    <div class="col2 mr-5 text-center lateloadfadeIn">
-    <a v-bind:href="item.link" target="blank" v-bind:alt="item.title"><img v-bind:data-src="item.img" v-bind:title="item.title" width="100px" height="100px" style="display:none;" /></a>
-    <p>{{item.title}}</p><p><span v-bind:class="GetClass(item)">{{GetName(item)}}</span></p>
-  </div>`
-})
+    <a v-bind:href="item.link" target="blank" v-bind:alt="item.title">
+       <img v-bind:data-src="item.img" v-bind:title="item.title" width="100px" height="100px" style="display:none;" />
+    </a>
+    <p>{{item.title}}</p>
+    <p>
+       <span v-bind:class="GetClass(item)">{{GetName(item)}}</span>
+    </p>
+  </div>`,
+});
 
-Vue.component('Work', {
-  props: ['job'],
+Vue.component("Work", {
+  props: ["job"],
   template: `  
     <div class="row justify-content-md-center">
      <div class="col-md-2 mt-3"><img class="ImgRoundCorner border-right shadow" v-lazy="job.img" v-bind:alt="job.name"></img></div>  
@@ -95,12 +128,16 @@ Vue.component('Work', {
       <h5>{{job.title}}, {{job.timeworked}}</h5>
       <p class="card-text">{{job.summary}}</p>
      </div>
-    </div>`
-})
+    </div>`,
+});
 
-Vue.component('Thought', {
-  props: ['item'],
-  methods: { getClass: function (size) { return "card shadow col-md-" + size + " mt-5" } },
+Vue.component("Thought", {
+  props: ["item"],
+  methods: {
+    getClass: function (size) {
+      return "card shadow col-md-" + size + " mt-5";
+    },
+  },
   template: `  
   <div :class="getClass(item.size)">
    <div class="card-header bg-transparent"><h2 class="h4">{{item.name}}</h2></div>
@@ -109,32 +146,45 @@ Vue.component('Thought', {
      <Knowledge v-for="item2 in item.items" v-bind:item="item2"></Knowledge>
     </div>   
    </div>
-  </div>`
-})
+  </div>`,
+});
 
-Vue.component('Project', {
-  props: ['project'],
+Vue.component("Project", {
+  props: ["project"],
   methods: {
     GetImage(img) {
       this.WebPSupport = SupportsWebp;
       switch (this.WebPSupport) {
-        case 1: return img;
-        case 0: return img + ".png";
+        case 1:
+          return img;
+        case 0:
+          return img + ".png";
         default:
-          let elem = document.createElement('canvas');
-          if (!!(elem.getContext && elem.getContext('2d')) && elem.toDataURL('image/webp').indexOf('data:image/webp') == 0) {
-            SupportsWebp = 1; return img;
-          } else { SupportsWebp = 0; return img + "png"; }
+          let elem = document.createElement("canvas");
+          if (
+            !!(elem.getContext && elem.getContext("2d")) &&
+            elem.toDataURL("image/webp").indexOf("data:image/webp") == 0
+          ) {
+            SupportsWebp = 1;
+            return img;
+          } else {
+            SupportsWebp = 0;
+            return img + "png";
+          }
       }
     },
     DetermButtonColor(ItemType) {
-      if (ItemType.toLowerCase() == "website") { return "btn btn-success shadow ml-2"; }
-      if (ItemType.toLowerCase() == "release") { return "btn btn-primary shadow ml-2"; }
+      if (ItemType.toLowerCase() == "website") {
+        return "btn btn-success shadow ml-2";
+      }
+      if (ItemType.toLowerCase() == "release") {
+        return "btn btn-primary shadow ml-2";
+      }
       return "btn btn-secondary shadow ml-2";
-    }
+    },
   },
   template: `
-  <div class="col mb-4 hoverItem">
+  <div class="col mb-4 hoverItem d-flex align-items-stretch">
    <div class="card shadow">
     <div>
      <img v-lazy="GetImage(project.image)"  style="height:250px;" class="card-img-top himg" v-bind:alt="project.Title"/>
@@ -150,10 +200,13 @@ Vue.component('Project', {
      </div>
     </div>
    </div>
-  </div>`
-})
+  </div>`,
+});
 /*Vue Items */
-backend = new Vue({ el: "#vbackend", data: { Ex: pageObject.Experence, WepPSupport: SupportsWebp } });
+backend = new Vue({
+  el: "#vbackend",
+  data: { Ex: pageObject.Experence, WepPSupport: SupportsWebp },
+});
 
 career = new Vue({
   el: "#career",
@@ -161,141 +214,184 @@ career = new Vue({
     Work: pageObject.Work,
     WepPSupport: SupportsWebp,
     highlights: pageObject.highlights,
-    school: pageObject.School
-  }
+    school: pageObject.School,
+  },
 });
 
 Nav = new Vue({
-  el: '#navItem',
+  el: "#navItem",
   methods: {
     validatePath(path) {
       let current = location.pathname.substring(1).toLowerCase();
-      if (current == "") { current = "index.html"; }
+      if (current == "") {
+        current = "index.html";
+      }
       if (path == current) return "active";
       return "";
-    }
-  }, data: { MenuItems: pageObject.menu }
+    },
+  },
+  data: { MenuItems: pageObject.menu },
 });
 
 blogs = new Vue({
-  el: '#Blogs',
+  el: "#Blogs",
   methods: {
     getImg(index) {
       let img = "./img/blog/blog" + index + ".webp";
       this.WebPSupport = SupportsWebp;
       switch (this.WebPSupport) {
-        case 1: return img;
-        case 0: return img + ".png";
+        case 1:
+          return img;
+        case 0:
+          return img + ".png";
         default:
-          let elem = document.createElement('canvas');
-          if (!!(elem.getContext && elem.getContext('2d')) && elem.toDataURL('image/webp').indexOf('data:image/webp') == 0) {
-            SupportsWebp = 1; return img;
-          } else { SupportsWebp = 0; return img + "png"; }
+          let elem = document.createElement("canvas");
+          if (
+            !!(elem.getContext && elem.getContext("2d")) &&
+            elem.toDataURL("image/webp").indexOf("data:image/webp") == 0
+          ) {
+            SupportsWebp = 1;
+            return img;
+          } else {
+            SupportsWebp = 0;
+            return img + "png";
+          }
       }
-    }
-    , GetItems() {
+    },
+    GetItems() {
       let M = Array.from(this.BlogItems);
       if (this.BlogItems.length > 3) {
         M = M.slice(0, 3);
       }
       var objArr = M.map(function (idx, i) {
         return {
-          title: idx.querySelector("title").innerHTML, link: idx.querySelector("link").innerHTML, timestamp: idx.querySelector("pubDate").innerHTML, index: i
+          title: idx.querySelector("title").innerHTML,
+          link: idx.querySelector("link").innerHTML,
+          timestamp: idx.querySelector("pubDate").innerHTML,
+          index: i,
         };
       });
       return objArr;
-    }
+    },
   },
   data: {
     BlogItems: [],
   },
   created() {
     fetch("https://dev.to/feed/liukonen")
-      .then(response => response.text())
-      .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-      .then(data => { this.BlogItems = data.querySelectorAll("item"); });
-  }
-
+      .then((response) => response.text())
+      .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+      .then((data) => {
+        this.BlogItems = data.querySelectorAll("item");
+      });
+  },
 });
-
 
 welcome = new Vue({ el: "#welcome", data: { item: pageObject.welcome } });
 About = new Vue({
   el: "#about",
   data: {
     Hobbies: pageObject.Hobbies,
-    Vol: pageObject.volunteer
-  }
-})
+    Vol: pageObject.volunteer,
+  },
+});
 
 vm = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
     Products: pageObject.projects,
-    WebPSupport: SupportsWebp
-  }
-})
+    WebPSupport: SupportsWebp,
+  },
+});
 
-vm.$Lazyload.$on('loaded', function ({ bindType, el, naturalHeight, naturalWidth, $parent, src, loading, error }, formCache) {
-  let T = $(el);
-  T.animate({ 'opacity': '1' }, 1500);
-})
+vm.$Lazyload.$on(
+  "loaded",
+  function (
+    { bindType, el, naturalHeight, naturalWidth, $parent, src, loading, error },
+    formCache
+  ) {
+    let T = $(el);
+    T.animate({ opacity: "1" }, 1500);
+  }
+);
 
 /**Vanilla JS */
 
-const lateLoadFadeInObserver = new IntersectionObserver((entries, lateLoadFadeInObserver) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      lateLoadFadeInObserver.unobserve(entry.target);
-      let I = $(entry.target);
-      let img = I.find("img");
-      let dsrc = img.attr("data-src");
-      img.attr("src", SupportsWebp != 1 ? dsrc + ".png" : dsrc);
-      img.fadeIn("slow");
-    }
-  });
-}, { threshold: 1, rootMargin: "0px 0px 0px 0px" });
-document.querySelectorAll(".lateloadfadeIn").forEach(entry => { lateLoadFadeInObserver.observe(entry); });
-
+const lateLoadFadeInObserver = new IntersectionObserver(
+  (entries, lateLoadFadeInObserver) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        lateLoadFadeInObserver.unobserve(entry.target);
+        let I = $(entry.target);
+        let img = I.find("img");
+        let dsrc = img.attr("data-src");
+        img.attr("src", SupportsWebp != 1 ? dsrc + ".png" : dsrc);
+        img.fadeIn("slow");
+      }
+    });
+  },
+  { threshold: 1, rootMargin: "0px 0px 0px 0px" }
+);
+document.querySelectorAll(".lateloadfadeIn").forEach((entry) => {
+  lateLoadFadeInObserver.observe(entry);
+});
 
 /*region Lazy Load */
 function preloadImg(img) {
   let src = img.getAttribute("data-src");
   if (src) {
-    if (SupportsWebp != 1) { img.src = src + ".png"; }
-    else { img.src = src; }
+    if (SupportsWebp != 1) {
+      img.src = src + ".png";
+    } else {
+      img.src = src;
+    }
   }
 }
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      preloadImg(entry.target);
-      imgObserver.unobserve(entry.target);
-    }
-  });
-}, { threshold: 1, rootMargin: "0px 0px 300px 0px" });
-document.querySelectorAll(".featurette-image").forEach(img => { imgObserver.observe(img); });
+const imgObserver = new IntersectionObserver(
+  (entries, imgObserver) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        preloadImg(entry.target);
+        imgObserver.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 1, rootMargin: "0px 0px 300px 0px" }
+);
+document.querySelectorAll(".featurette-image").forEach((img) => {
+  imgObserver.observe(img);
+});
 
-const bgObserver = new IntersectionObserver((entries, imgObserver) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      var image = entry.target;
-      image.classList.remove("lazy");
-      imgObserver.unobserve(entry.target);
-    }
-  });
-
-}, { threshold: 0});
-document.querySelectorAll(".lazy").forEach(item => bgObserver.observe(item));
+const bgObserver = new IntersectionObserver(
+  (entries, imgObserver) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        var image = entry.target;
+        image.classList.remove("lazy");
+        imgObserver.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0 }
+);
+document.querySelectorAll(".lazy").forEach((item) => bgObserver.observe(item));
 
 /*region fader */
-const faderObsever = new IntersectionObserver((entries, faderObsever) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) { $(entry.target).animate({ 'opacity': '1' }, 500); faderObsever.unobserve(entry.target); }
-  });
-}, { rootMargin: "0px 0px 0px 0px" });
-document.querySelectorAll(".fade-in").forEach(item => faderObsever.observe(item));
+const faderObsever = new IntersectionObserver(
+  (entries, faderObsever) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        $(entry.target).animate({ opacity: "1" }, 500);
+        faderObsever.unobserve(entry.target);
+      }
+    });
+  },
+  { rootMargin: "0px 0px 0px 0px" }
+);
+document
+  .querySelectorAll(".fade-in")
+  .forEach((item) => faderObsever.observe(item));
 /*end region fader */
 
 function ExpandText(me) {
@@ -305,29 +401,35 @@ function ExpandText(me) {
 
 function ExpandContent(item, content) {
   if (content.style.display === "block") {
-    item.innerHTML = '[+]';
+    item.innerHTML = "[+]";
     content.style.display = "none";
   } else {
-    item.innerHTML = '[-]';
+    item.innerHTML = "[-]";
     content.style.display = "block";
   }
-
 }
 
 /*region transparent Navbar */
 const header = document.querySelector("nav");
-const navObserver = new IntersectionObserver((entries, navObserver) => {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      header.classList.add("bg-dark");
-      header.classList.add("transition");
-    } else {
-      header.classList.remove("bg-dark");
-      header.classList.remove("transition");
-    }
-  });
-}, { rootMargin: "-50px 0px 0px 0px" });
+const navObserver = new IntersectionObserver(
+  (entries, navObserver) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        header.classList.add("bg-dark");
+        header.classList.add("transition");
+      } else {
+        header.classList.remove("bg-dark");
+        header.classList.remove("transition");
+      }
+    });
+  },
+  { rootMargin: "-50px 0px 0px 0px" }
+);
 navObserver.observe(document.querySelector(".bgimg-1"));
 
-$('body').scrollspy({ target: '.navbar' });
-$('.navbar-toggler').click(function() {console.log("hit"); header.classList.add("bg-dark"); header.classList.add("transition");});
+$("body").scrollspy({ target: ".navbar" });
+$(".navbar-toggler").click(function () {
+  console.log("hit");
+  header.classList.add("bg-dark");
+  header.classList.add("transition");
+});
