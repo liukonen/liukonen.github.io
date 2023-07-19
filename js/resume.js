64f8666304a6fc69d109250e7829bafc655d5dc1
luke liukonen <liukonen@gmail.com>
2023-07-18 21:04:57 -0500

    fix bulk of resume going to Handlerbars removed template based binding removed Jquery bumped bootstrap to 5.3 using CDN's over local todo: update data, fix images, new style, compress JS

--- js/resume.js
+++ js/resume.js
@@ -1,119 +1,105 @@
-var month = ["", "Jan","Feb","Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+var month = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
-function generateResume(){
+function generateResume() {
 
-let items = JSON.parse(getFile("./json/resume.json"));
+   getFile("./json/resume.json").then(items =>{
 
-//$.ajax({
-//  url:  "./resume.json",
-//    dataType: "text",	async: false,
-//  error: function(jqXHR, textStatus, error) {window.alert(error);},
-//  success: function(data, textStatus, jqXHR) {temp = data;}
-//}).then( async function(item){
-//let items = JSON.parse(item);
-//let arrayOfPromises = [
-        BuildExperence(items["work"]),
-        BuildSkills(items["skills"]),
-        BuildHeader(items["basics"]),
-        BuildEdu(items["education"]),
-        BuildBasic(items["basics"]),
-        $("#summaryItem").text(items["basics"]["summary"])
-  //  ];
-
-      console.log(items["basics"]["summary"]);
-      //await Promise.all(arrayOfPromises);
-//});
+  BuildExperence(items["work"]),
+    BuildSkills(items["skills"]),
+    BuildHeader(items["basics"]),
+    BuildEdu(items["education"]),
+    BuildBasic(items["basics"]),
+    document.getElementById("summaryItem").innerText = items["basics"]["summary"]
+  })
 }
 
 
-function getIconSM(name){
-switch (name) {
-  case "GitHub": return "fab fa-github-square";
+function BuildBasic(basicItem) {
+  basicBind("BasicItems", "basicsTemp", basicItem)
+}
+
+function buildBasicSM(item) {
+  return $("#basicsMediaTemp").tmpl(item);
+}
+
+
+function BuildHeader(basicItem) {
+  basicBind("HeaderItem", "headerTemp", basicItem)
+}
+
+function BuildEdu(eduItems) {
+  listBind('EduItems', 'eduTemp', eduItems)
+}
+function BuildSkills(skillsItems) {
+  listBind('SkillItems', 'skillsTemp', skillsItems)
+}
+
+function basicBind(ObjectTag, SourceTag, ObjectToBind) {
+  let objSource = document.getElementById(ObjectTag)
+  let source = document.getElementById(SourceTag).innerHTML
+  let template = Handlebars.compile(source)
+  objSource.innerHTML = template(ObjectToBind)
+}
+
+function listBind(ObjectTag, SourceTag, ObjectsToBind){
+  let source = document.getElementById(SourceTag).innerHTML
+  let temp = Handlebars.compile(source)
+  let MainContainer = document.getElementById(ObjectTag)
+  if (ObjectsToBind != null){
+  ObjectsToBind.forEach((item) => {
+    let html = temp(item)
+    MainContainer.insertAdjacentHTML('beforeend', html)
+  })}
+}
+
+function BuildExperence(experenceItems) {
+  listBind('ExperenceItems', 'experenceTemp', experenceItems)
+}
+
+
+Handlebars.registerHelper("socialMediaIcon", (media) => {
+  switch (media) {
+    case "GitHub": return "fab fa-github-square";
     case "LinkedIn": return "fab fa-linkedin";
     case "Facebook": return "fab fa-facebook-square";
-  default: return "far fa-user";
-}
+    default: return "far fa-user";
+  }
+})
 
-}
-
-function BuildBasic(basicItem){
-let Item = $("#basicsTemp").tmpl(basicItem);
-basicItem["profiles"].forEach(function(item){
-  Item.find("ul").append(buildBasicSM(item));
-});
-console.log(Item);
-$("#BasicItems").append(Item);
-return 1;
-}
-
-function buildBasicSM(item){
-return $("#basicsMediaTemp").tmpl(item);
-}
-
-
-function BuildHeader(basicItem){
-//let main = ;
-//let template = ;
-$("#headerTemp").tmpl(basicItem).appendTo($("#HeaderItem"));
-}
-
-function BuildEdu(eduItems){
-  let main = $("#EduItems");
-  let template = $("#eduTemp");
-  eduItems.forEach(function(item){
-    template.tmpl(item).appendTo(main);
-  });
-return 1;
-}
-function BuildSkills(skillsItems){
-let main = $("#SkillItems");
-let temp = $("#skillsTemp");
-skillsItems.forEach(function (skill){
-main.append(temp.tmpl(skill));
-
-});return 1;
-}
-
-function getImg(imgUrl){
-  let lType = imgUrl.toLowerCase();
-  if (lType == "c#"){return "csharp";}
-  if (lType == "vb.net"){return "vb";}
-  if (lType == "asp.net"){return "ASP";}
-  if (lType == "python"){return "python";}
-  if (lType.indexOf("scrum") > 0){return "scrum";}
-  if (lType.indexOf("jquery")> 0){return "javascript";}
-  if (lType.indexOf("sql") > 0){return "db";}
-  if (lType.indexOf("cognitive") > 0){ return "bot";}
-  if (lType.indexOf("bot") > 0){ return "bot";}
+Handlebars.registerHelper("ImageHelper", (media) =>{
+  let lType = media.toLowerCase();
+  if (lType == "c#")  return "csharp" 
+  if (lType == "vb.net")  return "vb" 
+  if (lType == "asp.net")  return "ASP" 
+  if (lType == "python")  return "python" 
+  if (lType.indexOf("scrum") > 0)  return "scrum" 
+  if (lType.indexOf("jquery") > 0)  return "javascript" 
+  if (lType.indexOf("sql") > 0)  return "db" 
+  if (lType.indexOf("cognitive") > 0)  return "bot" 
+  if (lType.indexOf("bot") > 0)  return "bot" 
   return "check";
-}
+})
 
-
-
-function BuildExperence(experenceItems){
-  let MainContainer = $("#ExperenceItems");
-  let template = $("#experenceTemp");
-  experenceItems.forEach(function(item){
-    let response = template.tmpl(item);
-    if (item["highlights"].length >=0){
-      item["highlights"].forEach(function(item) {
-        let li = document.createElement("li");
-        li.innerHTML = item;
-        response.find("ul").append(li);
-        });
-      }
-    response.appendTo(MainContainer);
-  });return 1;
-}
-
-
-function parseWorkDate(strDate){
-  if (strDate.length > 0){
+Handlebars.registerHelper("parseWorkDate", function (strDate) {
+  //function parseWorkDate(strDate){
+  if (strDate.length > 0) {
     let S = strDate.split("-");
     return month[parseInt(S[1])] + " " + S[0];
   } else {
     return "Present";
   }
-}
+})
 
+function getFile(fileUrl) {
+  return fetch(fileUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status + ' ' + response.statusText);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      window.alert(error);
+    });
+}
 generateResume();
