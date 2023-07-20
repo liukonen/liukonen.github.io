0ea8af19087e049d4d4cb2cbf937106d1d2716f7
luke liukonen <liukonen@gmail.com>
2023-07-19 21:40:36 -0500

    Switch from handlebars to native ES6 templates fix missing images Move styles to SASS Minify JS start improved styling

--- js/base.js
+++ js/base.js
@@ -14,29 +14,29 @@ let temp;
 
 
 
-/*! modernizr 3.6.0 (Custom Build) | MIT *
- * https://modernizr.com/download/?-createelementattrs_createelement_attrs-webp !*/
- !function(e,n,t){function A(e,n){return typeof e===n}function a(){var e,n,t,a,o,i,s;for(var l in r)if(r.hasOwnProperty(l)){if(e=[],n=r[l],n.name&&(e.push(n.name.toLowerCase()),n.options&&n.options.aliases&&n.options.aliases.length))for(t=0;t<n.options.aliases.length;t++)e.push(n.options.aliases[t].toLowerCase());for(a=A(n.fn,"function")?n.fn():n.fn,o=0;o<e.length;o++)i=e[o],s=i.split("."),1===s.length?Modernizr[s[0]]=a:(!Modernizr[s[0]]||Modernizr[s[0]]instanceof Boolean||(Modernizr[s[0]]=new Boolean(Modernizr[s[0]])),Modernizr[s[0]][s[1]]=a),u.push((a?"":"no-")+s.join("-"))}}function o(){return"function"!=typeof n.createElement?n.createElement(arguments[0]):p?n.createElementNS.call(n,"http://www.w3.org/2000/svg",arguments[0]):n.createElement.apply(n,arguments)}function i(e){var n=c.className,t=Modernizr._config.classPrefix||"";if(p&&(n=n.baseVal),Modernizr._config.enableJSClass){var A=new RegExp("(^|\\s)"+t+"no-js(\\s|$)");n=n.replace(A,"$1"+t+"js$2")}Modernizr._config.enableClasses&&(n+=" "+t+e.join(" "+t),p?c.className.baseVal=n:c.className=n)}function s(e,n){if("object"==typeof e)for(var t in e)f(e,t)&&s(t,e[t]);else{e=e.toLowerCase();var A=e.split("."),a=Modernizr[A[0]];if(2==A.length&&(a=a[A[1]]),"undefined"!=typeof a)return Modernizr;n="function"==typeof n?n():n,1==A.length?Modernizr[A[0]]=n:(!Modernizr[A[0]]||Modernizr[A[0]]instanceof Boolean||(Modernizr[A[0]]=new Boolean(Modernizr[A[0]])),Modernizr[A[0]][A[1]]=n),i([(n&&0!=n?"":"no-")+A.join("-")]),Modernizr._trigger(e,n)}return Modernizr}var r=[],l={_version:"3.6.0",_config:{classPrefix:"",enableClasses:!0,enableJSClass:!0,usePrefixes:!0},_q:[],on:function(e,n){var t=this;setTimeout(function(){n(t[e])},0)},addTest:function(e,n,t){r.push({name:e,fn:n,options:t})},addAsyncTest:function(e){r.push({name:null,fn:e})}},Modernizr=function(){};Modernizr.prototype=l,Modernizr=new Modernizr;var f,u=[];!function(){var e={}.hasOwnProperty;f=A(e,"undefined")||A(e.call,"undefined")?function(e,n){return n in e&&A(e.constructor.prototype[n],"undefined")}:function(n,t){return e.call(n,t)}}();var c=n.documentElement,p="svg"===c.nodeName.toLowerCase();Modernizr.addTest("createelementattrs",function(){try{return"test"==o('<input name="test" />').getAttribute("name")}catch(e){return!1}},{aliases:["createelement-attrs"]}),l._l={},l.on=function(e,n){this._l[e]||(this._l[e]=[]),this._l[e].push(n),Modernizr.hasOwnProperty(e)&&setTimeout(function(){Modernizr._trigger(e,Modernizr[e])},0)},l._trigger=function(e,n){if(this._l[e]){var t=this._l[e];setTimeout(function(){var e,A;for(e=0;e<t.length;e++)(A=t[e])(n)},0),delete this._l[e]}},Modernizr._q.push(function(){l.addTest=s}),Modernizr.addAsyncTest(function(){function e(e,n,t){function A(n){var A=n&&"load"===n.type?1==a.width:!1,o="webp"===e;s(e,o&&A?new Boolean(A):A),t&&t(n)}var a=new Image;a.onerror=A,a.onload=A,a.src=n}var n=[{uri:"data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA=",name:"webp"},{uri:"data:image/webp;base64,UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAABBxAR/Q9ERP8DAABWUDggGAAAADABAJ0BKgEAAQADADQlpAADcAD++/1QAA==",name:"webp.alpha"},{uri:"data:image/webp;base64,UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA",name:"webp.animation"},{uri:"data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=",name:"webp.lossless"}],t=n.shift();e(t.name,t.uri,function(t){if(t&&"load"===t.type)for(var A=0;A<n.length;A++)e(n[A].name,n[A].uri)})}),a(),delete l.addTest,delete l.addAsyncTest;for(var d=0;d<Modernizr._q.length;d++)Modernizr._q[d]();e.Modernizr=Modernizr}(window,document);
+// /*! modernizr 3.6.0 (Custom Build) | MIT *
+//  * https://modernizr.com/download/?-createelementattrs_createelement_attrs-webp !*/
+//  !function(e,n,t){function A(e,n){return typeof e===n}function a(){var e,n,t,a,o,i,s;for(var l in r)if(r.hasOwnProperty(l)){if(e=[],n=r[l],n.name&&(e.push(n.name.toLowerCase()),n.options&&n.options.aliases&&n.options.aliases.length))for(t=0;t<n.options.aliases.length;t++)e.push(n.options.aliases[t].toLowerCase());for(a=A(n.fn,"function")?n.fn():n.fn,o=0;o<e.length;o++)i=e[o],s=i.split("."),1===s.length?Modernizr[s[0]]=a:(!Modernizr[s[0]]||Modernizr[s[0]]instanceof Boolean||(Modernizr[s[0]]=new Boolean(Modernizr[s[0]])),Modernizr[s[0]][s[1]]=a),u.push((a?"":"no-")+s.join("-"))}}function o(){return"function"!=typeof n.createElement?n.createElement(arguments[0]):p?n.createElementNS.call(n,"http://www.w3.org/2000/svg",arguments[0]):n.createElement.apply(n,arguments)}function i(e){var n=c.className,t=Modernizr._config.classPrefix||"";if(p&&(n=n.baseVal),Modernizr._config.enableJSClass){var A=new RegExp("(^|\\s)"+t+"no-js(\\s|$)");n=n.replace(A,"$1"+t+"js$2")}Modernizr._config.enableClasses&&(n+=" "+t+e.join(" "+t),p?c.className.baseVal=n:c.className=n)}function s(e,n){if("object"==typeof e)for(var t in e)f(e,t)&&s(t,e[t]);else{e=e.toLowerCase();var A=e.split("."),a=Modernizr[A[0]];if(2==A.length&&(a=a[A[1]]),"undefined"!=typeof a)return Modernizr;n="function"==typeof n?n():n,1==A.length?Modernizr[A[0]]=n:(!Modernizr[A[0]]||Modernizr[A[0]]instanceof Boolean||(Modernizr[A[0]]=new Boolean(Modernizr[A[0]])),Modernizr[A[0]][A[1]]=n),i([(n&&0!=n?"":"no-")+A.join("-")]),Modernizr._trigger(e,n)}return Modernizr}var r=[],l={_version:"3.6.0",_config:{classPrefix:"",enableClasses:!0,enableJSClass:!0,usePrefixes:!0},_q:[],on:function(e,n){var t=this;setTimeout(function(){n(t[e])},0)},addTest:function(e,n,t){r.push({name:e,fn:n,options:t})},addAsyncTest:function(e){r.push({name:null,fn:e})}},Modernizr=function(){};Modernizr.prototype=l,Modernizr=new Modernizr;var f,u=[];!function(){var e={}.hasOwnProperty;f=A(e,"undefined")||A(e.call,"undefined")?function(e,n){return n in e&&A(e.constructor.prototype[n],"undefined")}:function(n,t){return e.call(n,t)}}();var c=n.documentElement,p="svg"===c.nodeName.toLowerCase();Modernizr.addTest("createelementattrs",function(){try{return"test"==o('<input name="test" />').getAttribute("name")}catch(e){return!1}},{aliases:["createelement-attrs"]}),l._l={},l.on=function(e,n){this._l[e]||(this._l[e]=[]),this._l[e].push(n),Modernizr.hasOwnProperty(e)&&setTimeout(function(){Modernizr._trigger(e,Modernizr[e])},0)},l._trigger=function(e,n){if(this._l[e]){var t=this._l[e];setTimeout(function(){var e,A;for(e=0;e<t.length;e++)(A=t[e])(n)},0),delete this._l[e]}},Modernizr._q.push(function(){l.addTest=s}),Modernizr.addAsyncTest(function(){function e(e,n,t){function A(n){var A=n&&"load"===n.type?1==a.width:!1,o="webp"===e;s(e,o&&A?new Boolean(A):A),t&&t(n)}var a=new Image;a.onerror=A,a.onload=A,a.src=n}var n=[{uri:"data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA=",name:"webp"},{uri:"data:image/webp;base64,UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAABBxAR/Q9ERP8DAABWUDggGAAAADABAJ0BKgEAAQADADQlpAADcAD++/1QAA==",name:"webp.alpha"},{uri:"data:image/webp;base64,UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA",name:"webp.animation"},{uri:"data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=",name:"webp.lossless"}],t=n.shift();e(t.name,t.uri,function(t){if(t&&"load"===t.type)for(var A=0;A<n.length;A++)e(n[A].name,n[A].uri)})}),a(),delete l.addTest,delete l.addAsyncTest;for(var d=0;d<Modernizr._q.length;d++)Modernizr._q[d]();e.Modernizr=Modernizr}(window,document);
 
-$(window).on('load', function() {
-  Modernizr.on('webp', function(result) {
-    if (result) {
-      console.log("Webp supported");
-      SupportsWebp = 1;
-    } else {
-      console.log("webp not supported, fallback to png");
-      SupportsWebp = 0;
-      let imgs = $("img").toArray();
-      imgs.forEach((img) =>{
-        var src = $(img).attr("src");
-        if (typeof src !== typeof undefined && src !== false && src != "" && src.substring(src.length -4) == "webp") {
-            $(img).attr("src", src + ".png");
-        }
-      });
-      $(".jumbotron").css("background-image", "url('../img/jumbotron.webp.png')");
-    }
-  });
-});
+// $(window).on('load', function() {
+//   Modernizr.on('webp', function(result) {
+//     if (result) {
+//       console.log("Webp supported");
+//       SupportsWebp = 1;
+//     } else {
+//       console.log("webp not supported, fallback to png");
+//       SupportsWebp = 0;
+//       let imgs = $("img").toArray();
+//       imgs.forEach((img) =>{
+//         var src = $(img).attr("src");
+//         if (typeof src !== typeof undefined && src !== false && src != "" && src.substring(src.length -4) == "webp") {
+//             $(img).attr("src", src + ".png");
+//         }
+//       });
+//       $(".jumbotron").css("background-image", "url('../img/jumbotron.webp.png')");
+//     }
+//   });
+// });
 
 if(navigator.appName.indexOf("Internet Explorer")!=-1 || navigator.userAgent.match(/Trident.*rv[ :]*11\./)){ window.location = "ie.html"; }
 
--- js/resume.js
+++ js/resume.js
@@ -1,94 +1,169 @@
 var month = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 function generateResume() {
-
-   getFile("./json/resume.json").then(items =>{
-
-  BuildExperence(items["work"]),
-    BuildSkills(items["skills"]),
-    BuildHeader(items["basics"]),
-    BuildEdu(items["education"]),
-    BuildBasic(items["basics"]),
-    document.getElementById("summaryItem").innerText = items["basics"]["summary"]
+  getFile("./json/resume.json").then(items => {
+    BuildExperence(items["work"]),
+      BuildSkills(items["skills"]),
+      BuildHeader(items["basics"]),
+      BuildEdu(items["education"]),
+      BuildBasic(items["basics"])
   })
 }
 
-
 function BuildBasic(basicItem) {
-  basicBind("BasicItems", "basicsTemp", basicItem)
+  const template = item => `
+   <div class="card text-white gradient-card">
+    <img class="card-img-top" src="img/original/${item.picture}" alt="me">
+    <div class="card-body">
+      ${item.summary}
+      <ul class="list-group custom-transparent-list text-white mt-2">
+        <li class="list-group-item d-flex justify-content-between align-items-start  custom-transparent-item">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold"><a href="tel:${item.phone}">${item.phone}</a></div>
+            Cellular
+          </div>
+          <i class="fa-solid fa-2x fa-phone"></i>      
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start  custom-transparent-item">
+        <div class="ms-2 me-auto">
+          <div class="fw-bold"><a href="mailto:${item.email}">${item.email}</a></div>
+          personal
+        </div>
+        <i class="fa-solid fa-2x fa-envelope"></i>      
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start  custom-transparent-item">
+        <div class="ms-2 me-auto">
+          <div class="fw-bold">${item.region}</div>
+          region I live in
+        </div>
+        <i class="fa-solid fa-2x fa-house"></i>      
+        </li>
+             ${buildprofiles(item.profiles)}
+      </ul>
+    <div>
+  </div>`
+  let response = template(basicItem)
+  document.getElementById("BasicItems").innerHTML = response
 }
 
-function buildBasicSM(item) {
-  return $("#basicsMediaTemp").tmpl(item);
+function buildprofiles(listItems) {
+  let str = []
+  const template = item => `
+  <li class="list-group-item d-flex justify-content-between align-items-start  custom-transparent-item">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold"><a href="${item.url}" target="_blank">@${item.username}</a></div>
+      ${item.network}
+    </div>
+    <i class="${socialMediaIcon(item.network)} fa-2x"></i>      
+  </li>`
+  listItems.forEach(element => {
+    str.push(template(element))
+  })
+  return str.join("")
 }
-
 
 function BuildHeader(basicItem) {
-  basicBind("HeaderItem", "headerTemp", basicItem)
+  var template = basicItem => `
+  <div class="lh-100"><h1 class="mb-0 text-white lh-100"> ${basicItem.name}</h1><p>
+  ${basicItem.headline}</p></div>`
+  document.getElementById("HeaderItem").innerHTML = template(basicItem)
 }
 
 function BuildEdu(eduItems) {
-  listBind('EduItems', 'eduTemp', eduItems)
+  let strings = []
+  const temp = data => `
+  <div>
+    <img src="img/original/${data.institution}.webp" width="32" height="32" alt="${data.institution}" class="left"><br />
+    <span class="lead">${data.area}</span><br/>
+    <span class="text-muted">${data.studyType}</span><br/>
+    <span class="lead">${data.institution}</span><br/>
+    <span class="text-muted">${parseWorkDate(data.startDate)} - ${parseWorkDate(data.endDate)}</span>
+  </div>`
+  eduItems.forEach(edu => { strings.push(temp(edu)) })
+  document.getElementById("EduItems").insertAdjacentHTML('beforeend', strings.join(""))
 }
+
 function BuildSkills(skillsItems) {
-  listBind('SkillItems', 'skillsTemp', skillsItems)
+  let listItems = []
+  const temp = d => `
+<li class="list-group-item d-flex justify-content-between align-items-center">
+<!-- <div class="progress">
+    <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${d.name}</div>
+  </div> -->
+<div><img src="/img/original/${ImageHelper(d.name)}.webp" height="16" width="16" alt="${d.name} image" />${d.name}</div>
+<span class="badge badge-dark badge-pill" title="${d.level} [${d.yearsOfExperience} years experence]">${d.level}</span>
+</li>`
+  skillsItems.forEach(element => {
+    listItems.push(temp(element))
+  });
+  document.getElementById("SkillItems").innerHTML = listItems.join("")
 }
 
-function basicBind(ObjectTag, SourceTag, ObjectToBind) {
-  let objSource = document.getElementById(ObjectTag)
-  let source = document.getElementById(SourceTag).innerHTML
-  let template = Handlebars.compile(source)
-  objSource.innerHTML = template(ObjectToBind)
-}
-
-function listBind(ObjectTag, SourceTag, ObjectsToBind){
+function listBind(ObjectTag, SourceTag, ObjectsToBind) {
   let source = document.getElementById(SourceTag).innerHTML
   let temp = Handlebars.compile(source)
   let MainContainer = document.getElementById(ObjectTag)
-  if (ObjectsToBind != null){
-  ObjectsToBind.forEach((item) => {
-    let html = temp(item)
-    MainContainer.insertAdjacentHTML('beforeend', html)
-  })}
+  if (ObjectsToBind != null) {
+    ObjectsToBind.forEach((item) => {
+      let html = temp(item)
+      MainContainer.insertAdjacentHTML('beforeend', html)
+    })
+  }
 }
 
 function BuildExperence(experenceItems) {
-  listBind('ExperenceItems', 'experenceTemp', experenceItems)
+  let items = []
+  let temp = d => `
+  <div class="media text-muted pt-3 lh-125 border-bottom border-gray">
+     <img class="" width="32" height="32" src="img/original/${d.company}.webp" alt="${d.company}" />
+     <div class="col">
+         <strong class="d-block">${d.company}</strong> - ${d.location}<br />
+
+         <p class="media-body pb-3 mb-0"> ${d.summary}
+             <ul>${generateHighlightlist(d.highlights)}</ul>
+         </p>
+     </div>
+ </div>
+  `
+  experenceItems.forEach(ex => {
+    items.push(temp(ex))
+  })
+  document.getElementById("ExperenceItems").insertAdjacentHTML('beforeend', items.join(""))
+  //document.getElementById("ExperenceItems").innerHTML = items.join("")
 }
 
+function generateHighlightlist(highlights) {
+  let temp = d => `<li>${d}</li>`
+  let response = []
+  highlights.forEach(highlight => {
+    response.push(temp(highlight))
+  })
+  return response.join("")
+}
 
-Handlebars.registerHelper("socialMediaIcon", (media) => {
+function socialMediaIcon(media) {
   switch (media) {
     case "GitHub": return "fab fa-github-square";
     case "LinkedIn": return "fab fa-linkedin";
     case "Facebook": return "fab fa-facebook-square";
     default: return "far fa-user";
   }
-})
+}
 
-Handlebars.registerHelper("ImageHelper", (media) =>{
+function ImageHelper(media) {
   let lType = media.toLowerCase();
-  if (lType == "c#")  return "csharp" 
-  if (lType == "vb.net")  return "vb" 
-  if (lType == "asp.net")  return "ASP" 
-  if (lType == "python")  return "python" 
-  if (lType.indexOf("scrum") > 0)  return "scrum" 
-  if (lType.indexOf("jquery") > 0)  return "javascript" 
-  if (lType.indexOf("sql") > 0)  return "db" 
-  if (lType.indexOf("cognitive") > 0)  return "bot" 
-  if (lType.indexOf("bot") > 0)  return "bot" 
+  if (lType == "c#") return "csharp"
+  if (lType == "vb.net") return "vb"
+  if (lType == "asp.net") return "ASP"
+  if (lType == "python") return "python"
+  if (lType.indexOf("scrum") > 0) return "scrum"
+  if (lType.indexOf("jquery") > 0) return "javascript"
+  if (lType.indexOf("sql") > 0) return "db"
+  if (lType.indexOf("cognitive") > 0) return "bot"
+  if (lType.indexOf("bot") > 0) return "bot"
   return "check";
-})
 
-Handlebars.registerHelper("parseWorkDate", function (strDate) {
-  //function parseWorkDate(strDate){
-  if (strDate.length > 0) {
-    let S = strDate.split("-");
-    return month[parseInt(S[1])] + " " + S[0];
-  } else {
-    return "Present";
-  }
-})
+}
 
 function getFile(fileUrl) {
   return fetch(fileUrl)
@@ -102,4 +177,14 @@ function getFile(fileUrl) {
       window.alert(error);
     });
 }
-generateResume();
+
+function parseWorkDate(strDate) {
+  console.log(strDate)
+  if (strDate.length > 0) {
+    let S = strDate.split("-");
+    return month[parseInt(S[1])] + " " + S[0];
+  } else {
+    return "Present";
+  }
+}
+generateResume()
