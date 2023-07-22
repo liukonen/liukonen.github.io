be4598d6966e62a7cbe1a7509265375873a1d14d
luke liukonen <liukonen@gmail.com>
2023-07-22 14:13:14 -0500

    removed Jquery templates for native templates removed Jquery removed Bootstrap js removed modernizr removed base.js dependencies caching files in sesson state for faster responses and less load rewrote much of the diary.js core logic replaced showdown with marked minimized code added year to copywrite

--- js/diary.js
+++ js/diary.js
@@ -1,290 +1,226 @@
-var liWeeks;
-var ActiveWeek = 0;
-var MaxWeek = 0;
-var lastName; var LastLink;
-const MagicDate = new Date(2020,2,29);
-const search = document.querySelector('#search');
-const results = document.querySelector('#searchresults');
-const SearchItems = JSON.parse( getFile("https://liukonen.dev/furlough/keywords.json"));
-var ActiveWeeks = [];
-var AllWeeks = [];
-var days;
+var ActiveWeek = 0
+var MaxWeek = 0
+
+const MagicDate = new Date(2020, 2, 29);
+const SearchItems = JSON.parse(getFile("https://liukonen.dev/furlough/keywords.json"))
+var ActiveWeeks = []
+var AllWeeks = []
 
 
-const ContentObserver = new IntersectionObserver((entries, 
-  ContentObserver)=> {
-    entries.forEach(entry => {
-      if (entry.isIntersecting){
-        let S = entry.target.getAttribute("data-src");
-        let item = S.split("|");
-        console.log(item);
-        entry.target.innerHTML =  ExtractBlogItem(item[0], item[1]);
-        ContentObserver.unobserve(entry.target);  
-      }
-});
-}, {threshold:1, rootMargin:"0px 0px 0px 0px"});
 
-function baseUrl(){
-  let item = new URL(furloughDiary);
-  return item.protocol + "//" + item.host;
-}
-
-function generateNavs(){
-  let weeksString = getFile(furloughDiary);
-  let navTemp = $("#templateNav");
-  let MainContainer = $("#ulNav");
-  AllWeeks = liLinkParser2(weeksString);
-  ActiveWeeks = AllWeeks.slice(AllWeeks.length -4);
-  ActiveWeeks.forEach(item =>{navTemp.tmpl(item).appendTo(MainContainer); lastName = item.Name; LastLink=item.Link;});
-
-  $("#li-"+ RmName(lastName)).attr("class", "page-item active");
-  MaxWeek = parseInt(RmName(lastName));
-  console.log(lastName);
-  PopulateWeek(LastLink, lastName);
-}
-
-function adjustNavs(direction){
-  let navTemp = $("#templateNav");
-  let MainContainer = $("#ulNav");
-  MainContainer.empty();
-  if (direction == 0){
-
-    let currentLow = ActiveWeeks[0].ItemNumber;
-    let newLow = (currentLow-4 >= 1) ? currentLow -4 : 1;
-    console.log(newLow-1, 4);
-    ActiveWeeks = AllWeeks.slice(newLow-1, newLow + 3);
-    console.log(AllWeeks);
-
-  }else{
-     let newlow = ActiveWeeks[ActiveWeeks.length -1].ItemNumber;
-     let length = (newlow + 4 >= MaxWeek) ? MaxWeek - newlow : 4;
-    ActiveWeeks = AllWeeks.slice(newlow, newlow + length);    
-  }
-  ActiveWeeks.forEach(item =>{navTemp.tmpl(item).appendTo(MainContainer);});
-  SetActive(ActiveWeek);
-}
-
-function PopulateWeek(weekUrl, name){
-
-  let weekString = getFile(baseUrl() + weekUrl);
-  let blogTemp = $("#dTemp");
-  let placeholder = $("#PlaceHolderItem");
-  placeholder.empty();
-  ActiveWeek = parseInt(RmName(name));
-  days = liLinkParser2(weekString);
-   days.reverse().forEach(item=>{blogTemp.tmpl(item).appendTo(placeholder)});
-  BindDynamic();
-  SetActive(ActiveWeek);
-}
-
-function PopulateSearchResults(SearchResults){
-  console.log("Hit");
-  let blogTemp = $("#dTemp");
-  let placeholder = $("#PlaceHolderItem");
-  let RI = [];
-  ActiveWeek = -1;
-  //let counter = 1;
-  placeholder.empty();
-  SearchResults.forEach(sItem => {
-    let NewURL = "./furlough/days/" + sItem.substring(0, sItem.length -2) + "html";
-    let counter = parseInt(sItem.substring(3).substring(0,2));
-    RI.push({Name: FakeNewName(sItem), Link:NewURL, ItemNumber: counter});
-    counter++;
-  });
-  //LoadContent(RI);
-  RI.forEach(item => {blogTemp.tmpl(item).appendTo(placeholder)});
-  BindDynamic();
-  SetActive(-1);  
-}
-
-function BindDynamic(){
-  const DynamicContent = document.querySelectorAll(".EItem");
-  ContentObserver.disconnect();
-  console.log(DynamicContent);
-  DynamicContent.forEach(item=>{ContentObserver.observe(item);});
-}
-function FakeNewName(name)
-{
-  let X = name[0].toUpperCase() + name.substring(1);
-  let Y = X.substring(0, 3) + " " + X.substring(3);
-  return Y.substring(0, Y.length -3);
-}
-
-function SetActive(activeWeek)
-{
-    var i;
-    ActiveWeeks.forEach(item =>{
-      $("#btn-" + item.ItemNumber).attr("class", (item.ItemNumber == activeWeek ? "nav-link active": "nav-link"));
-    })
-    $("#prev").show();
-    $("#next").show();
-
-  if (ActiveWeeks[0].ItemNumber == 1){$("#prev").hide();}
-  if (ActiveWeeks[ActiveWeeks.length-1].ItemNumber == MaxWeek){$("#next").hide();}
-}
-
-function liLinkParser2(file){
-    let response = [];
-    let startIndex = file.indexOf("<ul>");
-    let lastIndex = file.indexOf("</ul>");
-    let sstring = file.substring(startIndex, lastIndex);
-    let i = 0;
-    let counter = 1;
-    while (i >=0){
-      i = sstring.indexOf("href", i);
-      if (i > 0){
-        i +=6;
-        let Y = sstring.indexOf('"', i);
-        let link = sstring.substring(i, Y);
-        i = Y;
-        i = sstring.indexOf(">", i) +1;
-        Y = sstring.indexOf("<", i);
-        let text = sstring.substring(i,Y);
-        response.push({Name: text, Link:link, ItemNumber: counter});
-        counter++;
-        }
-      }
-    return response;
-  }
-
-function RmName(name){return name.replace("Week ", "");}
-
-function CalcDate(ItemNumber, WeekNumber){
-  if (WeekNumber == -1) {
-    console.log(ItemNumber);
-    let calcWeek = parseInt(ItemNumber / 5) + 1;
-    let CalcDay = parseInt(ItemNumber % 5) + 1;
-    return CalcDate(CalcDay, calcWeek); 
-  }
-  return (addDays(MagicDate, (WeekNumber * 7)+ ItemNumber));
-}
-
-function ReplaceAll(fullText, replaceText, replaceWith) {
-  var r = new RegExp(replaceText, "igm");
-  return fullText.replace(r, replaceWith)
-}
-
-function ExtractBlogItem(dayUrl){ return ExtractBlogItem(dayUrl, "");}
-
-function ExtractBlogItem(dayUrl, name){
-    let dayString = getFile(baseUrl() + dayUrl.substring(0, dayUrl.length -4) + "md");
-    let teststring = dayString.replace("#", "").trim();
-
-    console.log(teststring.substring(0, 10));
-    if (teststring.startsWith(name)){dayString = teststring.substring(name.length);}
-    
-    let sstring = ReplaceAll(dayString, "# ", "### ");
-    console.log(sstring);
-    let converter = new showdown.Converter();
-    let html = converter.makeHtml(sstring);
-    return html;
-}
-
-function addDays(date, days) {
-    const copy = new Date(Number(date))
-    copy.setDate(date.getDate() + days)
-    return copy
-  }
-
-
-  function SearchFind2() {SearchFind(search.value.trim().toLowerCase());}
-
-function SearchFind(searchString)
-{
-    let ResponseItem  = SearchItems.filter(x=> x.keyword == searchString)[0] || null;
-    if (ResponseItem != null) PopulateSearchResults(ResponseItem.files);
-}
-
-
-function GenerateKeywords()
-{
-    let template = $("#tempKeywordList");
-    for(i = 0; 5 > i; i++) {template.tmpl(SearchItems[i]).appendTo($("#ulKeywords"));}
-}
-
-/*region - navbar transparent effect*/
-const header = document.querySelector("nav");
-const Jumbo = document.querySelector(".jumbotron");
-const navOptions = {rootMargin:"-50px 0px 0px 0px"};
-
-const navObserver = new IntersectionObserver((entries, navObserver) =>{
-  entries.forEach(entry =>{
-    if (!entry.isIntersecting){
-      console.log("dark");
-      header.classList.add("bg-dark");
-      header.classList.add("transition");
-    }else{
-      console.log("trans");
-      header.classList.remove("bg-dark");
-      header.classList.remove("transition");
+const ContentObserver = new IntersectionObserver((entries,
+  ContentObserver) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      let S = entry.target.getAttribute("data-src")
+      let item = S.split("|")
+      entry.target.innerHTML = ExtractBlogItem(item[0], item[1])
+      ContentObserver.unobserve(entry.target)
     }
-  });
-}, {rootMargin:"-50px 0px 0px 0px"});
+  })
+}, { threshold: 1, rootMargin: "0px 0px 0px 0px" })
 
-//load
-console.log("observe");
-console.log(header);
-console.log(Jumbo);
-navObserver.observe(Jumbo);
-/*end region navbar transparent effect */
+function SearchFind2() { SearchFind(search.value.trim().toLowerCase()) }
 
-generateNavs();
-GenerateKeywords();
+function SearchFind(searchString) {
+  let ResponseItem = SearchItems.filter(x => x.keyword == searchString)[0] || null
+  if (ResponseItem != null) PopulateSearchResults(ResponseItem.files)
+}
 
-function showModal(obj, e){
-  console.log("hit");
-  //obj.preventDefault();
-  let url = obj.getAttribute("href");
-  try{
-  if (window.innerWidth >= 576){
-    
-    $("#staticBackdropLabel").text($(this).text());
-    $("#dynamicModal iframe").attr("src", url);
-    $("#dynamicModal").modal("show");
-    $("#aSite").attr("href", url);
+
+const navTemplate = data => `<li class="nav-item" id="li-${RmName(data.Name)}"><a class="nav-link" id="btn-${RmName(data.Name)}" onclick="PopulateWeek('${data.Link}', '${data.Name}')">${RmName(data.Name)}</a></li>`
+const itemTemplate = d =>`<div class="col-md-12">
+ <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow h-md-250 position-relative">
+     <div class="col-md-12 p-4 d-flex flex-column position-static">
+         <a href="${d.Link}" target="_blank">
+             <h3 class="mb-0">${d.Name}</h3>
+         </a>
+         <div class="mb-1 text-muted">${CalcDate(d.ItemNumber).toDateString()}</div>
+         <p class="card-text mb-auto">
+             <div class="EItem" data-src="${d.Link}|${d.Name}"></div>
+         </p>
+     </div>
+ </div>
+</div>`
+
+const innerBind = (template, dataArray, elementName) => document.getElementById(elementName).insertAdjacentHTML('beforeend', dataArray.map(item => template(item)).join(''))
+const itemBind = (template, dataArray,activeWeek,elementName) => document.getElementById(elementName).insertAdjacentHTML('beforeend', dataArray.map(item => template(item,activeWeek)).join(''))
+
+
+const addDays = (date, days) => new Date(date.getTime() + days * 24 * 60 * 60 * 1000)
+const ReplaceAll = (fullText, replaceText, replaceWith) => fullText.replace(new RegExp(replaceText, "igm"), replaceWith)
+const RmName = (name) => name.replace("Week ", "")
+const furloughDiary = "https://liukonen.dev/furlough/index.html"
+const baseUrl = () => `${new URL(furloughDiary).protocol}//${new URL(furloughDiary).host}`
+
+const searchInput = document.getElementById('search')
+const searchResults = document.getElementById('searchresults')
+
+
+
+generateNavs()
+GenerateKeywords()
+
+
+
+
+
+
+//NEW REWORKED CODE GOES BELOW THIS LINE
+
+function PopulateWeek(weekUrl, name) {
+  let weekString = getFile(baseUrl() + weekUrl)
+  const placeHolder = document.getElementById("PlaceHolderItem")
+  placeHolder.innerHTML = ""
+  ActiveWeek = parseInt(RmName(name))
+  let currentDays = listLinkParser(weekString)
+  innerBind(itemTemplate,currentDays.reverse(), "PlaceHolderItem")
+  BindDynamic()
+  SetActive(ActiveWeek)
+}
+
+function PopulateSearchResults(SearchResults) {
+  const placeholder = document.getElementById("PlaceHolderItem")
+  ActiveWeek = -1
+  placeholder.innerHTML = ""
+  const RI = SearchResults.map((sItem) => ({
+    Name: FakeNewName(sItem),
+    Link: `./furlough/days/${sItem.substring(0, sItem.length - 2)}html`,
+    ItemNumber: parseInt(sItem.substring(3, 5)) 
+  }))
+  innerBind(itemTemplate, RI, "PlaceHolderItem")
+  BindDynamic()
+  SetActive(-1)
+}
+
+function generateNavs() {
+  let weeksString = getFile(furloughDiary)
+  AllWeeks = listLinkParser(weeksString)
+  ActiveWeeks = AllWeeks.slice(AllWeeks.length - 4)
+  innerBind(navTemplate, ActiveWeeks, "ulNav")
+  const lastAttribute = ActiveWeeks[ActiveWeeks.length - 1]
+  document.getElementById("li-" + RmName(lastAttribute.Name)).className = "page-item active"
+  MaxWeek = parseInt(RmName(lastAttribute.Name))
+  PopulateWeek(lastAttribute.Link, lastAttribute.Name)
+}
+
+function adjustNavs(direction) {
+  let MainContainer = document.getElementById("ulNav")
+  MainContainer.innerHTML = ""
+  if (direction == 0) {
+    let currentLow = ActiveWeeks[0].ItemNumber
+    let newLow = (currentLow - 4 >= 1) ? currentLow - 4 : 1
+    ActiveWeeks = AllWeeks.slice(newLow - 1, newLow + 3)
+  } else {
+    let newlow = ActiveWeeks[ActiveWeeks.length - 1].ItemNumber
+    let length = (newlow + 4 >= MaxWeek) ? MaxWeek - newlow : 4
+    ActiveWeeks = AllWeeks.slice(newlow, newlow + length)
   }
-  else {var win = window.open(url, '_blank'); win.focus();}
-  e.preventDefault();
-  return false;
+  innerBind(navTemplate, ActiveWeeks, "ulNav")
+  SetActive(ActiveWeek)
 }
-catch{}//do nothing
+function BindDynamic() {
+  const DynamicContent = document.querySelectorAll(".EItem")
+  DynamicContent.forEach(item => { ContentObserver.observe(item) })
 }
 
-search.addEventListener('keyup', (event) =>
-{
+function FakeNewName(name) {
+  return name.replace(/^./, match => match.toUpperCase()).replace(/(.{3})/, "$1 ")
+}
 
-	if (event.keyCode === 13)
-	{
-        SearchFind2();
-	}
-	else
-	{
-		while (results.firstChild) results.removeChild(results.lastChild);
-		let counter = 0;
-		let searchString = search.value.trim().toLowerCase();
-		SearchItems.forEach(searchItem =>
-		{
-			if (counter != 6)
-			{
-				let S = searchItem.keyword;
-				if (searchItem.keyword.startsWith(searchString))
-				{
-					counter++;
-					let item = document.createElement("option");
-					item.text = searchItem.keyword;
-					results.appendChild(item);
-				}
-			}
-		});
-	}
-});
+function SetActive(activeWeek) {
+  ActiveWeeks.forEach(item => {
+    document.querySelector("#btn-" + item.ItemNumber).classList.toggle("active", item.ItemNumber === activeWeek)
+  })
+  const prevButton = document.querySelector("#prev")
+  const nextButton = document.querySelector("#next")
+  prevButton.style.display = ActiveWeeks[0].ItemNumber === 1 ? "none" : "block"
+  nextButton.style.display = ActiveWeeks[ActiveWeeks.length - 1].ItemNumber === MaxWeek ? "none" : "block"
+}
 
-$("#search").on('input', function() {
-  var val = this.value;
-  console.log(val);
-  if($('#searchresults option').filter(function() {
-    console.log(this);
-    console.log(this.value);
-      return this.value.toLowerCase() === val.toLowerCase();        
-  }).length) {SearchFind(this.value); }
-});
+function CalcDate(ItemNumber) {
+  return (addDays(MagicDate, (ActiveWeek * 7) + ItemNumber))
+}
+
+function listLinkParser(file) {
+  const response = []
+  const ulRegex = /<ul>(.*?)<\/ul>/gs
+  const liRegex = /<a\s+href="(.*?)">(.*?)<\/a>/gs
+  let ulMatch
+  while ((ulMatch = ulRegex.exec(file))) {
+    const [, liContent] = ulMatch
+    let liMatch
+    let counter = 1
+    while ((liMatch = liRegex.exec(liContent))) {
+      const [, link, text] = liMatch
+      response.push({ Name: text, Link: link, ItemNumber: counter })
+      counter++
+    }
+  }
+
+  return response
+}
+
+function ExtractBlogItem(dayUrl) { return ExtractBlogItem(dayUrl, "") }
+
+function ExtractBlogItem(dayUrl, name) {
+  const dayString = getFile(baseUrl() + dayUrl.substring(0, dayUrl.length - 4) + "md")
+  const teststring = dayString.replace("#", "").trim()
+  let modifiedDayString = teststring.startsWith(name) ? teststring.substring(name.length) : dayString
+  const sstring = ReplaceAll(modifiedDayString, "# ", "### ")
+  return marked.parse(sstring)
+}
+
+function GenerateKeywords() {
+  const template = data => `<li class="list-group-item d-flex justify-content-between align-items-center">
+    <a onclick="SearchFind('${data.keyword}')">${data.keyword}</a>
+      <span class="badge text-bg-primary">${data.count}</span>
+    </li>`
+  innerBind(template, SearchItems.slice(0, 5), "ulKeywords")
+}
+
+searchInput.addEventListener('input', function () {
+  const val = this.value.toLowerCase()
+  const optionFound = Array.from(searchResults.options).some(option => option.value.toLowerCase() === val)
+  if (optionFound) SearchFind(this.value)
+})
+
+searchInput.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    SearchFind2()
+  }
+  else {
+    searchResults.innerHTML = ""
+    let searchString = searchInput.value.trim().toLowerCase()
+    const filteredItems = SearchItems.filter(s => s.keyword.startsWith(searchString)).slice(0, 6)
+    const template = data => `<option>${data.keyword}</option>`
+    innerBind(template, filteredItems, "searchresults")
+  }
+})
+
+const fileCache = {}
+
+function getFile(fileUrl) {
+let response = ""
+  const cachedFile = sessionStorage.getItem(fileUrl)
+  if (cachedFile) return cachedFile
+
+  const xhr = new XMLHttpRequest()
+  xhr.open("GET", fileUrl, false)
+
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      const data = xhr.responseText
+      sessionStorage.setItem(fileUrl, data)
+      response = data
+    } else {
+      window.alert(`Failed to load file: ${xhr.status} ${xhr.statusText}`)
+    }
+  }
+
+  xhr.onerror = function () { window.alert("Network error occurred while loading the file.") }
+  xhr.send()
+  return response
+}
+
+document.getElementById("year").innerHTML = new Date().getFullYear()
